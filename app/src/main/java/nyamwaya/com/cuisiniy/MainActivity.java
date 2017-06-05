3e6cd39949f82b613a5c39e48c45470b6958ad30
nyamwaya <alecksonnyamwaya@gmail.com>
2017-06-05 10:20:02 -0500

    created bottom nav instance

--- app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
+++ app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
@@ -1,13 +1,52 @@
 package nyamwaya.com.cuisiniy;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textSearch;
+    private TextView textCamera;
+    private TextView textAccount;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textSearch = (TextView) findViewById(R.id.text_search);
+        textCamera = (TextView) findViewById(R.id.text_camera);
+        textAccount = (TextView) findViewById(R.id.text_account);
+
+        bottomNavigationView =(BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_search:
+                        textSearch.setVisibility(View.VISIBLE);
+                        textCamera.setVisibility(View.GONE);
+                        textAccount.setVisibility(View.GONE);
+                        break;
+                    case R.id.action_camera:
+                        textSearch.setVisibility(View.GONE);
+                        textCamera.setVisibility(View.VISIBLE);
+                        textAccount.setVisibility(View.GONE);
+                        break;
+                    case R.id.action_account:
+                        textSearch.setVisibility(View.GONE);
+                        textCamera.setVisibility(View.GONE);
+                        textAccount.setVisibility(View.VISIBLE);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
