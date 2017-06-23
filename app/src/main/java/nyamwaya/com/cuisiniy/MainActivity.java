4021e217801dff7635233ad1eb36f4c0965a6bc2
nyamwaya <alecksonnyamwaya@gmail.com>
2017-06-23 14:19:53 -0500

    frame layout and fragment trans

--- app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
+++ app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
@@ -2,6 +2,7 @@ package nyamwaya.com.cuisiniy;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -21,19 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mSearchText = (TextView) findViewById(R.id.text_search);
-        mCameraText = (TextView)(findViewById(R.id.text_camera));
-        mAccountText = (TextView)(findViewById(R.id.text_account));
+        mCameraText = (TextView) (findViewById(R.id.text_camera));
+        mAccountText = (TextView) (findViewById(R.id.text_account));
 
         renderBottomView();
 
     }
 
-    private void renderBottomView(){
+    private void renderBottomView() {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.action_search:
                         mSearchText.setVisibility(View.VISIBLE);
                         mCameraText.setVisibility(View.GONE);
@@ -53,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void fragmentTracsaction() {
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.your_placeholder, new FooFragment());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 
 }
