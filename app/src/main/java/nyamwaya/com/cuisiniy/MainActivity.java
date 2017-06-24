22a5649c268a55480d647f384b165988e2041528
nyamwaya <alecksonnyamwaya@gmail.com>
2017-06-23 21:59:25 -0500

    working camera fragment

--- app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
+++ app/src/main/java/nyamwaya/com/cuisiniy/MainActivity.java
@@ -1,33 +1,78 @@
 package nyamwaya.com.cuisiniy;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.github.florent37.camerafragment.CameraFragment;
+import com.github.florent37.camerafragment.configuration.Configuration;
+import com.github.florent37.camerafragment.listeners.CameraFragmentControlsAdapter;
+import com.github.florent37.camerafragment.listeners.CameraFragmentStateAdapter;
+import com.github.florent37.camerafragment.listeners.CameraFragmentVideoRecordTextAdapter;
+import com.github.florent37.camerafragment.widgets.CameraSettingsView;
+import com.github.florent37.camerafragment.widgets.CameraSwitchView;
+import com.github.florent37.camerafragment.widgets.FlashSwitchView;
+import com.github.florent37.camerafragment.widgets.MediaActionSwitchView;
+import com.github.florent37.camerafragment.widgets.RecordButton;
+
+import java.io.File;
+
 import nyamwaya.com.cuisiniy.ui.AccountFragment;
-import nyamwaya.com.cuisiniy.ui.CameraFragment;
 import nyamwaya.com.cuisiniy.ui.HomeFragment;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mSearchText;
-    private TextView mCameraText;
-    private TextView mAccountText;
+    public static final String FRAGMENT_TAG = "camera";
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    //Camera related views
+    private CameraSettingsView cameraSettingsView;
+    private CameraSwitchView cameraSwitchView;
+    private View cameraLayout;
+    private FlashSwitchView flashSwitchView;
+    private RecordButton recordButton;
+    private MediaActionSwitchView mediaActionSwitchView;
+    private CameraSettingsView settingsView;
+
+    private TextView recordDurationText;
+    private TextView recordSizeText;
+
+    //End camera related views
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        renderCameraView();
         renderBottomView();
 
     }
+
+    private void renderCameraView() {
+        cameraSwitchView = (CameraSwitchView) findViewById(R.id.front_back_camera_switcher);
+        cameraLayout = findViewById(R.id.cameraLayout);
+        flashSwitchView = (FlashSwitchView) findViewById(R.id.flash_switch_view);
+        recordButton = (RecordButton) findViewById(R.id.record_button);
+        mediaActionSwitchView = (MediaActionSwitchView) findViewById(R.id.photo_video_camera_switcher);
+        recordDurationText = (TextView) findViewById(R.id.record_duration_text);
+        recordSizeText = (TextView) findViewById(R.id.record_size_mb_text);
+        settingsView = (CameraSettingsView) findViewById(R.id.settings_view);
+
+
+    }
+
 
     private void renderBottomView() {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -37,10 +82,9 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_home:
                         fragmentTracsaction(new HomeFragment());
-
                         break;
                     case R.id.action_camera:
-                        fragmentTracsaction(new CameraFragment());
+                        addCamera();
                         break;
                     case R.id.action_account:
                         fragmentTracsaction(new AccountFragment());
@@ -55,10 +99,182 @@ public class MainActivity extends AppCompatActivity {
         // Begin the transaction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         // Replace the contents of the container with the new fragment
-        ft.replace(R.id.your_placeholder, fragment);
-        // or ft.add(R.id.your_placeholder, new FooFragment());
-        // Complete the changes added above
-        ft.commit();
+        if (ft != null) {
+            ft.replace(R.id.homefragments, fragment);
+            // or ft.add(R.id.your_placeholder, new FooFragment());
+            // Complete the changes added above
+            ft.commit();
+        }
     }
 
+    public void addCamera() {
+        cameraLayout.setVisibility(View.VISIBLE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        final CameraFragment cameraFragment = CameraFragment.newInstance(new Configuration.Builder()
+                .setCamera(Configuration.CAMERA_FACE_REAR).build());
+        fragmentTracsaction(cameraFragment);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content, cameraFragment, FRAGMENT_TAG)
+                .commitAllowingStateLoss();
+
+        if (cameraFragment != null) {
+            //cameraFragment.setResultListener(new CameraFragmentResultListener() {
+            //    @Override
+            //    public void onVideoRecorded(String filePath) {
+            //        Intent intent = PreviewActivity.newIntentVideo(MainActivity.this, filePath);
+            //        startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+            //    }
+//
+            //    @Override
+            //    public void onPhotoTaken(byte[] bytes, String filePath) {
+            //        Intent intent = PreviewActivity.newIntentPhoto(MainActivity.this, filePath);
+            //        startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+            //    }
+            //});
+
+            cameraFragment.setStateListener(new CameraFragmentStateAdapter() {
+
+                @Override
+                public void onCurrentCameraBack() {
+                    cameraSwitchView.displayBackCamera();
+                }
+
+                @Override
+                public void onCurrentCameraFront() {
+                    cameraSwitchView.displayFrontCamera();
+                }
+
+                @Override
+                public void onFlashAuto() {
+                    flashSwitchView.displayFlashAuto();
+                }
+
+                @Override
+                public void onFlashOn() {
+                    flashSwitchView.displayFlashOn();
+                }
+
+                @Override
+                public void onFlashOff() {
+                    flashSwitchView.displayFlashOff();
+                }
+
+                @Override
+                public void onCameraSetupForPhoto() {
+                    mediaActionSwitchView.displayActionWillSwitchVideo();
+
+                    recordButton.displayPhotoState();
+                    flashSwitchView.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onCameraSetupForVideo() {
+                    mediaActionSwitchView.displayActionWillSwitchPhoto();
+
+                    recordButton.displayVideoRecordStateReady();
+                    flashSwitchView.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void shouldRotateControls(int degrees) {
+                    ViewCompat.setRotation(cameraSwitchView, degrees);
+                    ViewCompat.setRotation(mediaActionSwitchView, degrees);
+                    ViewCompat.setRotation(flashSwitchView, degrees);
+                    ViewCompat.setRotation(recordDurationText, degrees);
+                    ViewCompat.setRotation(recordSizeText, degrees);
+                }
+
+                @Override
+                public void onRecordStateVideoReadyForRecord() {
+                    recordButton.displayVideoRecordStateReady();
+                }
+
+                @Override
+                public void onRecordStateVideoInProgress() {
+                    recordButton.displayVideoRecordStateInProgress();
+                }
+
+                @Override
+                public void onRecordStatePhoto() {
+                    recordButton.displayPhotoState();
+                }
+
+                @Override
+                public void onStopVideoRecord() {
+                    recordSizeText.setVisibility(View.GONE);
+                    //cameraSwitchView.setVisibility(View.VISIBLE);
+                    settingsView.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onStartVideoRecord(File outputFile) {
+                }
+            });
+
+            cameraFragment.setControlsListener(new CameraFragmentControlsAdapter() {
+                @Override
+                public void lockControls() {
+                    cameraSwitchView.setEnabled(false);
+                    recordButton.setEnabled(false);
+                    settingsView.setEnabled(false);
+                    flashSwitchView.setEnabled(false);
+                }
+
+                @Override
+                public void unLockControls() {
+                    cameraSwitchView.setEnabled(true);
+                    recordButton.setEnabled(true);
+                    settingsView.setEnabled(true);
+                    flashSwitchView.setEnabled(true);
+                }
+
+                @Override
+                public void allowCameraSwitching(boolean allow) {
+                    cameraSwitchView.setVisibility(allow ? View.VISIBLE : View.GONE);
+                }
+
+                @Override
+                public void allowRecord(boolean allow) {
+                    recordButton.setEnabled(allow);
+                }
+
+                @Override
+                public void setMediaActionSwitchVisible(boolean visible) {
+                    mediaActionSwitchView.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+                }
+            });
+
+            cameraFragment.setTextListener(new CameraFragmentVideoRecordTextAdapter() {
+                @Override
+                public void setRecordSizeText(long size, String text) {
+                    recordSizeText.setText(text);
+                }
+
+                @Override
+                public void setRecordSizeTextVisible(boolean visible) {
+                    recordSizeText.setVisibility(visible ? View.VISIBLE : View.GONE);
+                }
+
+                @Override
+                public void setRecordDurationText(String text) {
+                    recordDurationText.setText(text);
+                }
+
+                @Override
+                public void setRecordDurationTextVisible(boolean visible) {
+                    recordDurationText.setVisibility(visible ? View.VISIBLE : View.GONE);
+                }
+            });
+        }
+    }
 }
