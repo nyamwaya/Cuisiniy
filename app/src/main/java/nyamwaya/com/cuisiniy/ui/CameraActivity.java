18b9acf4d33132a5b785d5af481211baf1d85e0f
nyamwaya <alecksonnyamwaya@gmail.com>
2017-07-03 18:59:37 -0500

    imported R class

--- app/src/main/java/nyamwaya/com/cuisiniy/ui/CameraActivity.java
+++ app/src/main/java/nyamwaya/com/cuisiniy/ui/CameraActivity.java
@@ -3,6 +3,8 @@ package nyamwaya.com.cuisiniy.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import nyamwaya.com.cuisiniy.R;
+
 public class CameraActivity extends AppCompatActivity {
 
     @Override
