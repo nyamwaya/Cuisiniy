b488096295da8f4bb9846b6df8bfaa922e02249e
jafam <jafamefune@gmail.com>
2018-08-29 15:30:41 -0500

    Changed profile icon and toolbar to settings toolbar.

--- app/src/main/java/nyamwaya/com/cuisiniy/fragments/ProfileFragment.java
+++ app/src/main/java/nyamwaya/com/cuisiniy/fragments/ProfileFragment.java
@@ -3,11 +3,11 @@ package nyamwaya.com.cuisiniy.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -21,23 +21,30 @@ public class ProfileFragment extends Fragment {
     private Toolbar mToolbar;
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
 
-    public static ProfileFragment newInstance() {
-        ProfileFragment fragment = new ProfileFragment();
-        return fragment;
-    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        mToolbar= view.findViewById(R.id.toolbar);
+        mToolbar = view.findViewById(R.id.profileToolbar);
         mCollapsingToolbarLayout= view.findViewById(R.id.collapsing_toolbar);
 
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar_default, menu);
 
 
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 }
 
